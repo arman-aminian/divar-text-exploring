d67d3de4dfefe350470a7c5444db69e53b5ea59f
Arman Aminian <arman.aminian78@gmail.com>
2022-10-17 19:19:48 +0330

    feat: divar posts spider added.

---
+++ crawler/divar_data_collection/divar_data_collection/spiders/posts_spider.py
@@ -0,0 +1,34 @@
+import scrapy
+from crawler.utils.get_tokens import get_tokens
+
+url = 'https://divar.ir/v/-/{post_token}'
+
+
+class PostsSpider(scrapy.Spider):
+    name = 'divar'
+    start_urls = [url.format(post_token=token) for token in get_tokens(1650392836073764, 1, n_pages=2)]
+
+    def parse(self, response, **kwargs):
+        informations = response.css('div span.kt-group-row-item__value::text')
+
+        area = int(informations[0].extract())
+        construction = int(informations[1].extract())
+        rooms = int(informations[2].extract())
+
+        warehouse = False if 'ندارد' in informations[3].extract() else True
+        parking = False if 'ندارد' in informations[4].extract() else True
+        elevator = False if 'ندارد' in informations[5].extract() else True
+
+        address = response.css('div div.kt-page-title__subtitle--responsive-sized::text').extract()
+        price = response.css('div p.kt-unexpandable-row__value::text').extract_first()
+
+        yield {
+            'Area': area,
+            'Construction': construction,
+            'Room': rooms,
+            'Warehouse': warehouse,
+            'Parking': parking,
+            "Elevator": elevator,
+            'Address': address,
+            'Price': price
+        }
